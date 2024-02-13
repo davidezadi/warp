f8b64229a64de266060bc000d0de1ee528ab872e
davidezadi <davidezadi@gmail.com>
2024-02-13 08:17:48 +0330

    Update nimbaha-arvan-all.js

--- nimbaha-arvan-all.js
+++ nimbaha-arvan-all.js
@@ -1,75 +1,33 @@
-
 {
-    "log":{
-        "access":"/dev/null",
-        "error":"/dev/null",
-        "loglevel":"none"
-    },
-    
- "dns": {
-    "fakeip": {
-      "enabled": true,
-      "inet4_range": "198.18.0.0/15",
-      "inet6_range": "fc00::/18"
-    },
-    "independent_cache": true,
-    "rules": [
-      {
-        "disable_cache": true,
-        "inbound": [
-          "tun-in"
-        ],
-        "server": "dns-fake"
-      }
-    ],
-    "servers": [
-      {
-        "address": "tcp://8.8.8.8",
-        "address_resolver": "dns-direct",
-        "strategy": "prefer_ipv4",
-        "tag": "dns-remote"
-      },
-      {
-        "address": "tcp://1.1.1.1",
-        "address_resolver": "dns-local",
-        "detour": "direct",
-        "strategy": "prefer_ipv4",
-        "tag": "dns-direct"
-      },
-      {
-        "address": "local",
-        "detour": "direct",
-        "tag": "dns-local"
-      },
-      {
-        "address": "rcode://success",
-        "tag": "dns-block"
-      },
-      {
-        "address": "fakeip",
-        "strategy": "ipv4_only",
-        "tag": "dns-fake"
-      }
-    ]
+  "log": {
+    "access": "/dev/null",
+    "error": "/dev/null",
+    "loglevel": "none"
   },
-  
-       "routing": {
-        
-          "domainStrategy": "AsIs",
-          "rules": [
-            {
-                "type": "field",
-                "ip": [
-                    "geoip:private"
-                ],
-                "outboundTag": "block"
-            }
-        ]
-    } ,
+  "dns": {
+    "tag": "dns",
     
-
-"inbounds":[
-        {
+    "hosts": {
+      "cloudflare-dns.com": [
+        "172.67.73.38",
+        "104.19.155.92",
+        "172.67.73.163",
+        "104.18.155.42",
+        "104.16.124.175",
+        "104.16.248.249",
+        "104.16.249.249",
+        "104.26.13.8"        
+      ],
+      "domain:youtube.com": [
+        "google.com"
+      ]
+    },
+    "servers": [
+        "https://cloudflare-dns.com/dns-query"
+               ]
+  },
+  "inbounds": [
+            {
             "port":80,
             "protocol":"vless",
             "settings":{
@@ -85,19 +43,19 @@
                         "dest":3001
                     },
                     {
-                        "path":"/vless",
+                        "path":"/Fuckyou-Fuckyou-Fuckyou-vless",
                         "dest":3002
                     },
                     {
-                        "path":"/vmess",
+                        "path":"/Fuckyou-Fuckyou-Fuckyou-vmess",
                         "dest":3003
                     },
                     {
-                        "path":"/trojan",
+                        "path":"/Fuckyou-Fuckyou-Fuckyou-trojan",
                         "dest":3004
                     },
                     {
-                        "path":"/shadowsocks",
+                        "path":"/Fuckyou-Fuckyou-Fuckyou-shadowsocks",
                         "dest":3005
                     }
                 ]
@@ -108,7 +66,7 @@
         },
         {
             "port":3001,
-            "listen":"0.0.0.0",
+            "listen":"127.0.0.1",
             "protocol":"vless",
             "settings":{
                 "clients":[
@@ -119,13 +77,15 @@
                 "decryption":"none"
             },
             "streamSettings":{
+        "sockopt": {
+                    "domainStrategy": "UseIP"},
                 "network":"ws",
                 "security":"none"
             }
         },
         {
             "port":3002,
-            "listen":"0.0.0.0",
+            "listen":"127.0.0.1",
             "protocol":"vless",
             "settings":{
                 "clients":[
@@ -140,22 +100,26 @@
                 "network":"ws",
                 "security":"none",
                 "wsSettings":{
-                    "path":"/vless"
+                    "path":"/Fuckyou-Fuckyou-Fuckyou-vless"
                 }
             },
             "sniffing":{
                 "enabled":true,
                 "destOverride":[
                     "http",
-                    "tls"
+                    "tls",
+                    "quic"
                 ],
                 "metadataOnly":false
             }
         },
         {
             "port":3003,
-            "listen":"0.0.0.0",
+            "listen":"127.0.0.1",
             "protocol":"vmess",
+                        "streamSettings": {
+        "sockopt": {
+                    "domainStrategy": "UseIP"}},
             "settings":{
                 "clients":[
                     {
@@ -167,14 +131,15 @@
             "streamSettings":{
                 "network":"ws",
                 "wsSettings":{
-                    "path":"/vmess"
+                    "path":"/Fuckyou-Fuckyou-Fuckyou-vmess"
                 }
             },
             "sniffing":{
                 "enabled":true,
                 "destOverride":[
                     "http",
-                    "tls"
+                    "tls",
+                    "quic"
                 ],
                 "metadataOnly":false
             }
@@ -194,21 +159,38 @@
                 "network":"ws",
                 "security":"none",
                 "wsSettings":{
-                    "path":"/trojan"
-                }
+                    "path":"/Fuckyou-Fuckyou-Fuckyou-trojan"
+                             } },
+     "sockopt": {
+        "tcpMaxSeg": 2048,
+          "tcpFastOpen": false,
+          "mark": 0,
+          "tcpKeepAliveInterval": 1000,
+          "tcpKeepAliveIdle": 0,
+          "tcpUserTimeout": 10000,
+          "V6Only": false,
+          "tcpWindowClamp":  80,
+        "tproxy": "off",
+       "domainStrategy": "AsIs",
+        "tcpcongestion": "bbr",
+        "tcpNoDelay": true
             },
             "sniffing":{
                 "enabled":true,
                 "destOverride":[
                     "http",
-                    "tls"
+                    "tls",
+                    "quic"
                 ],
                 "metadataOnly":false
             }
         },
         {
             "port":3005,
-            "listen":"0.0.0.0",
+            "listen":"127.0.0.1",
+                        "streamSettings": {
+        "sockopt": {
+                    "domainStrategy": "UseIP"}},
             "protocol":"shadowsocks",
             "settings":{
                 "clients":[
@@ -222,61 +204,170 @@
             "streamSettings":{
                 "network":"ws",
                 "wsSettings":{
-                    "path":"/shadowsocks"
+                    "path":"/Fuckyou-Fuckyou-Fuckyou-shadowsocks"
                 }
             },
             "sniffing":{
                 "enabled":true,
                 "destOverride":[
                     "http",
-                    "tls"
+                    "tls",
+                    "quic"
                 ],
                 "metadataOnly":false
             }
-        }
-    ],
-    
-
-    "outbounds": [
-        {
-            "protocol": "freedom",
-            "tag": "direct"
         },
-        {
-            "protocol": "blackhole",
-            "tag": "block"
-        }
-         ],
-         
- "route": 
- {
-    "auto_detect_interface": true,
-    "rules": [
-      {
-        "outbound": "dns-out",
-        "port": [
-          53
-        ]
+    
+    {
+
+      "protocol": "socks",
+      "tag": "socks-in",
+      "domainOverride": [
+        "http",
+        "tls"
+                        ],
+      "listen": "127.0.0.1",
+      "port": 10808,
+                  "streamSettings": {
+        "sockopt": {
+                    "domainStrategy": "UseIP"}},
+      "settings": {
+        "auth": "noauth",
+        "udp": true,
+        "userLevel": 8
       },
-      {
-        "inbound": [
-          "dns-in"
-        ],
-        "outbound": "dns-out"
-      },
-      {
-        "ip_cidr": [
-          "224.0.0.0/3",
-          "ff00::/8"
-        ],
-        "outbound": "block",
-        "source_ip_cidr": [
-          "224.0.0.0/3",
-          "ff00::/8"
+      "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
         ]
       }
-    ]
-  }
+    },
+    {
+      "protocol": "http",
+      "tag": "http-in",
+      
+      "listen": "127.0.0.1",
+      "port": 10809,
+            "streamSettings": {
+        "sockopt": {
+        "domainStrategy": "UseIP"} },
+      "settings": {
+        "userLevel": 8
+      },
+      "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
+        ]
+      }
+    },
+        {
+      "listen": "127.0.0.1",
+      "port": 10853,
+      "protocol": "dokodemo-door",
+            "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
+        ]
+      },
+      "settings": {
+        "address": "1.1.1.1",
+        "network": "tcp,udp",
+        "port": 53
+      },
+    "streamSettings": {
+        "sockopt": {
+        "domainStrategy": "UseIP"} 
+            },
+      "tag": "dns-in"
+    }
+  ],
+  
+  
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "tag": "fragment-out",
+      "domainStrategy": "UseIP",
+      "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
+        ]
+      },
+      "settings": {
+        "fragment": {
+          "packets": "tlshello",
+          "length": "63-129",
+          "interval": "10-12"
+        }
+      },
+      "streamSettings": {
+        "sockopt": {
+                    "domainStrategy": "UseIP",
+          "tcpMaxSeg": 2048,
+          "tcpNoDelay": true,
+          "mark": 0,
+          "tcpKeepAliveInterval": 1000,
+          "tcpKeepAliveIdle": 0,
+          "tcpUserTimeout": 10000,
+          "tcpcongestion": "bbr",
+          "V6Only": false,
+          "tcpWindowClamp": 80,
+        "tproxy": "off",
+          "tcpFastOpen": false
+        }
+      }
+    },
+    {
+      "protocol": "dns",
+      "tag": "dns-out"
+    }
 
+  ],
+  
+  
+  "policy": {
+    "levels": {
+      "8": {
+        "handshake": 4,
+        "connIdle": 300,
+        "uplinkOnly": 0,
+        "downlinkOnly": 0,
+        "statsUserUplink": true,
+        "statsUserDownlink": true,
+        "bufferSize": 512
+      }
+    },
+    "system": {
+      "statsInboundUplink": true,
+      "statsInboundDownlink": true,
+      "statsOutboundUplink": true,
+      "statsOutboundDownlink": true
+    }
+  },
+  "routing": {
+    "domainStrategy": "AsIs",
+    "rules": [
+      {
+        "inboundTag": [
+          "socks-in",
+          "http-in",
+          "dns-in"
+        ],
+        "type": "field",
+        "port": "0-65535",
+        "outboundTag": "fragment-out",
+        "enabled": true
+      }
+    ],
+    "strategy": "rules"
+  },
+  "stats": {}
 }
-EOF
